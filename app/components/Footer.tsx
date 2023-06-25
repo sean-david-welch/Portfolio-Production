849782e865cebc630d2d21918b5ec38530f62218
Sean Welch <seanwelch51199@icloud.com>
2023-06-25 17:31:53 -0300

    styling for footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,55 +1,12 @@
-import Link from 'next/link';
+import { FooterButtons, FooterInformation } from './FooterContent';
 import styles from './styles/Footer.module.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faHome,
-    faCartPlus,
-    faDiagramProject,
-    faUser,
-    faAddressCard,
-    faCircleInfo,
-} from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
-                <ul className={styles.footerList}>
-                    <button className={styles.btn}>
-                        <Link className={styles.navItem} href={'/'}>
-                            <FontAwesomeIcon icon={faHome} size="sm" /> Home
-                        </Link>
-                    </button>
-                    <button className={styles.btn}>
-                        <Link className={styles.navItem} href={'/dashboard'}>
-                            <FontAwesomeIcon icon={faAddressCard} size="sm" />{' '}
-                            Dashboard
-                        </Link>
-                    </button>
-                    <button className={styles.btn}>
-                        <Link className={styles.navItem} href={'/about'}>
-                            <FontAwesomeIcon icon={faCircleInfo} size="sm" />{' '}
-                            About
-                        </Link>
-                    </button>
-
-                    <button className={styles.btn}>
-                        <Link className={styles.navItem} href={'/projects'}>
-                            <FontAwesomeIcon
-                                icon={faDiagramProject}
-                                size="sm"
-                            />{' '}
-                            Projects
-                        </Link>
-                    </button>
-                    <button className={styles.btn}>
-                        <Link className={styles.navItem} href={'/products'}>
-                            <FontAwesomeIcon icon={faCartPlus} size="sm" />{' '}
-                            Products
-                        </Link>
-                    </button>
-                </ul>
+                <FooterButtons />
+                <FooterInformation />
             </div>
         </footer>
     );
---
+++ app/components/FooterContent.tsx
@@ -0,0 +1,77 @@
+import styles from './styles/Footer.module.css';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome,
+    faCartPlus,
+    faDiagramProject,
+    faAddressCard,
+    faCircleInfo,
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+export const FooterInformation = () => {
+    return (
+        <div className={styles.footerInfo}>
+            <Image src={'/favicon.ico'} alt={'Logo'} height={100} width={100} />
+            <div className={styles.infoContent}>
+                <h1 className={styles.mainHeading}>
+                    Email: seanwelch51199@icloud.com
+                </h1>
+                <ul className={styles.linksList}>
+                    <Link
+                        href={'https://github.com/sean-david-welch'}
+                        target="_blank">
+                        <FontAwesomeIcon icon={faGithub} size={'xl'} />
+                        Github
+                    </Link>
+                    <Link
+                        href={
+                            'https://www.linkedin.com/in/sean-welch-3988191a5/'
+                        }
+                        target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} size={'xl'} />
+                        LinkedIn
+                    </Link>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export const FooterButtons = () => {
+    return (
+        <ul className={styles.footerList}>
+            <button className={styles.btn}>
+                <Link className={styles.navItem} href={'/'}>
+                    <FontAwesomeIcon icon={faHome} size="sm" /> Home
+                </Link>
+            </button>
+            <button className={styles.btn}>
+                <Link className={styles.navItem} href={'/dashboard'}>
+                    <FontAwesomeIcon icon={faAddressCard} size="sm" /> Dashboard
+                </Link>
+            </button>
+            <button className={styles.btn}>
+                <Link className={styles.navItem} href={'/about'}>
+                    <FontAwesomeIcon icon={faCircleInfo} size="sm" /> About
+                </Link>
+            </button>
+
+            <button className={styles.btn}>
+                <Link className={styles.navItem} href={'/projects'}>
+                    <FontAwesomeIcon icon={faDiagramProject} size="sm" />{' '}
+                    Projects
+                </Link>
+            </button>
+            <button className={styles.btn}>
+                <Link className={styles.navItem} href={'/products'}>
+                    <FontAwesomeIcon icon={faCartPlus} size="sm" /> Products
+                </Link>
+            </button>
+        </ul>
+    );
+};
