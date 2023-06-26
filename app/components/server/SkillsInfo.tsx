52132f1f941a025d6f9c41f4eb66bb1ae3aec6a0
Sean Welch <seanwelch51199@icloud.com>
2023-06-26 17:11:17 -0300

    finished tech section

--- app/components/server/SkillsInfo.tsx
+++ app/components/server/SkillsInfo.tsx
@@ -1,12 +1,22 @@
 import styles from '../styles/Info.module.css';
+import TechItems from '../client/techView';
+import TechnologiesForm from '../client/techForm';
+
+import { prisma } from '@/lib/primsa';
+import { getSessionAndUser } from '@/app/utils/apiUtils';
 
 export const SkillsDisplay = async () => {
+    const { user } = await getSessionAndUser();
+    const technologies = await prisma.technologies.findMany();
+
     return (
         <section id="skills">
             <div className={styles.SkillsDisplay}>
                 <h1 className={styles.mainHeading}>My Expertise</h1>
                 <h2 className={styles.heading}>Skills & Technologies Used:</h2>
             </div>
+            <TechItems technologies={technologies} />
+            {user && user.role === 'ADMIN' && <TechnologiesForm />}
         </section>
     );
 };
