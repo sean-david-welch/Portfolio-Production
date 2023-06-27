4478b55099b49ccac4485b4aec70c116cd922b73
Sean Welch <seanwelch51199@icloud.com>
2023-06-27 14:10:20 -0300

    added product detail page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,30 @@
+import { getSessionAndUser } from '@/app/utils/apiUtils';
 import styles from '../styles/Products.module.css';
+import { prisma } from '@/lib/primsa';
+import { ProductInfo } from './Product';
 
-const ProductDetail = async () => {
+interface Props {
+    params: { id: string };
+}
+
+const ProductDetail = async ({ params }: Props) => {
+    const { user } = await getSessionAndUser();
+
+    const product = await prisma.product.findUnique({
+        where: { id: params.id },
+    });
+
+    if (!product)
+        return (
+            <section id={styles.productDetail}>
+                <h1>No Product Found</h1>
+            </section>
+        );
+
     return (
-        <section id="product-detail">
-            <h1>Product</h1>
+        <section id={styles.productDetail}>
+            <h1 className={styles.mainHeading}>Product: {product.name}</h1>
+            <ProductInfo product={product} user={user} />
         </section>
     );
 };
