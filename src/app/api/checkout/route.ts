1451fe97bb932d8e4ba894ec6e07d3aa55dfc34d
Sean Welch <seanwelch51199@icloud.com>
2023-06-29 09:48:40 -0300

    tax included

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -30,6 +30,7 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
                             description: product.description,
                         },
                         unit_amount: product.price * 100,
+                        tax_behaviour: 'inclusive',
                     },
                     quantity: 1,
                 },
